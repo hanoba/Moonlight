6e1d2b5205f614e6c2d6989c598b1534c3918fab
Harald Bauer <harald.n.bauer@web.de>
2022-04-24 09:14:08 +0200

    added bin file

---
+++ upload.bat
@@ -0,0 +1,43 @@
+@echo off
+set BOSSAC=C:\Users\Harald\AppData\Local\Arduino15\packages\arduino\tools\bossac\1.6.1-arduino/bossac.exe
+set BIN=Moonlight.ino.bin
+::set BIN=C:\Users\Harald\AppData\Local\Temp\arduino_build_764037\Moonlight.ino.bin
+:: set BIN=C:\Users\Harald\AppData\Local\Temp\arduino_build_783022/Moonlight.ino.bin
+:: Usage: bossac.exe [OPTION...] [FILE]
+:: Basic Open Source SAM-BA Application (BOSSA) Version 1.6.1-arduino
+:: Flash programmer for Atmel SAM devices.
+:: Copyright (c) 2011-2012 ShumaTech (http://www.shumatech.com)
+:: 
+:: Examples:
+::   bossac -e -w -v -b image.bin   # Erase flash, write flash with image.bin,
+::                                  # verify the write, and set boot from flash
+::   bossac -r0x10000 image.bin     # Read 64KB from flash and store in image.bin
+:: 
+:: Options:
+::   -e, --erase           erase the entire flash (keep the 8KB of bootloader for SAM Dxx)
+::   -w, --write           write FILE to the flash; accelerated when
+::                         combined with erase option
+::   -r, --read[=SIZE]     read SIZE from flash and store in FILE;
+::                         read entire flash if SIZE not specified
+::   -v, --verify          verify FILE matches flash contents
+::   -p, --port=PORT       use serial PORT to communicate to device;
+::                         default behavior is to auto-scan all serial ports
+::   -b, --boot[=BOOL]     boot from ROM if BOOL is 0;
+::                         boot from FLASH if BOOL is 1 [default];
+::                         option is ignored on unsupported devices
+::   -c, --bod[=BOOL]      no brownout detection if BOOL is 0;
+::                         brownout detection is on if BOOL is 1 [default]
+::   -t, --bor[=BOOL]      no brownout reset if BOOL is 0;
+::                         brownout reset is on if BOOL is 1 [default]
+::   -l, --lock[=REGION]   lock the flash REGION as a comma-separated list;
+::                         lock all if not given [default]
+::   -u, --unlock[=REGION] unlock the flash REGION as a comma-separated list;
+::                         unlock all if not given [default]
+::   -s, --security        set the flash security flag
+::   -i, --info            display device information
+::   -d, --debug           print debug messages
+::   -h, --help            display this help text
+::   -U, --force_usb_port=true/false override USB port autodetection
+::   -R, --reset           reset CPU (if supported)
+%BOSSAC% -i --port=COM5 -U false -e -w -b %BIN% -R 
+pause
