9ad2d33e678f07f7181c2c90ce2496db5043b6e8
Harald <harald.n.bauer@web.de>
2024-07-07 08:27:39 +0200

    correct config.py

--- amcp/config.py
+++ amcp/config.py
@@ -1,6 +1,3 @@
-mapFileName='maps.json'
-exportFileName='export.json'
-linearSpeed='0.5'
 # constant configuration parameters that cannot be changed via the amcp GUI
 sonarEnable=1
 sonarObstacleDist=10
