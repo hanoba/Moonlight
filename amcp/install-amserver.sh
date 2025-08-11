22b793aa7acc4febf8e0fa03d01aca31d36da048
Harald Bauer <harald.n.bauer@web.de>
2025-08-11 08:57:25 +0200

    bugfix in install-amserver.sh

--- amcp/install-amserver.sh
+++ amcp/install-amserver.sh
@@ -1,5 +1,5 @@
 sudo cp amserver.service /etc/systemd/system/
 sudo systemctl daemon-reexec      # oder daemon-reload bei Änderung
-sudo systemctl enable meinservice
-sudo systemctl start meinservice
+sudo systemctl enable amserver
+sudo systemctl start amserver
 
